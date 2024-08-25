65b1894b407d4b2cb13c6b22d66a53932852ea05
DeMarco <rodrigodemvs@gmail.com>
2024-08-25 21:06:01 +0200

    Created Microchip Studio project for tuner FW

---
+++ DMH_VCO_40106_Firmware/DMH_VCO_40106_Firmware/main.c
@@ -0,0 +1,18 @@
+/*
+ * DMH_VCO_40106_Firmware.c
+ *
+ * Created: 25/08/2024 20:57:13
+ * Author : Draycon
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
