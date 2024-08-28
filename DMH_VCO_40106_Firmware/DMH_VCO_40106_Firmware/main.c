a0957adbf7b8a769c2dac66b9c38f303586cbf56
DeMarco <rodrigodemvs@gmail.com>
2024-08-28 23:04:31 +0200

    Created init_display and delay_s functions

--- DMH_VCO_40106_Firmware/DMH_VCO_40106_Firmware/main.c
+++ DMH_VCO_40106_Firmware/DMH_VCO_40106_Firmware/main.c
@@ -13,10 +13,50 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
+void delay_s (uint8_t t)
+{
+	uint8_t i;
+
+	for (i = 0; i < 10*t; i++)
+		_delay_ms(100);
+}
+
+void init_display (void)
+{
+	PORTA &= ~_BV(SEG_DP);
+	PORTD &= 0x00;
+	
+	delay_s(150);
+	PORTD &= _BV(SEG_F);
+	_delay_ms(150);
+	PORTD &= _BV(SEG_A);
+	_delay_ms(150);
+	PORTD &= _BV(SEG_B);
+	_delay_ms(150);
+	PORTD &= _BV(SEG_C);
+	_delay_ms(150);
+	PORTD &= _BV(SEG_D);
+	_delay_ms(150);
+	PORTD &= _BV(SEG_E);
+	_delay_ms(150);
+	PORTD &= _BV(SEG_G);
+	_delay_ms(150);
+	PORTD &= ~_BV(SEG_G);
+	PORTA |= _BV(SEG_DP);
+	_delay_ms(500);
+	PORTD &= 0xEF;
+	_delay_ms(500);
+	PORTA &= ~_BV(SEG_DP);
+}
 
 void display_note (uint8_t note)
 {
-	
+	switch(note)
+	{
+		case NOTE_C:
+			PORTD = _BV(SEG_A) | _BV(SEG_D) | _BV(SEG_E) | _BV(SEG_F) & ~_BV(SEG_B) & ~_BV(SEG_C) & ~_BV(SEG_G);
+		
+	}
 	
 }
 
@@ -30,7 +70,10 @@ void ioinit (void)
 
 int main(void)
 {
-    /* Replace with your application code */
+    
+	
+	init_display();
+	
     while (1) 
     {
     }
