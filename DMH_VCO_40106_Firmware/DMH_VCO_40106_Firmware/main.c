d05e75c8c2d38143c23bdfb8ef6427b7c78fa1b4
DeMarco <rodrigodemvs@gmail.com>
2024-09-01 14:19:24 +0200

    Added all notes to display_note function

--- DMH_VCO_40106_Firmware/DMH_VCO_40106_Firmware/main.c
+++ DMH_VCO_40106_Firmware/DMH_VCO_40106_Firmware/main.c
@@ -23,9 +23,11 @@ void delay_s (uint8_t t)
 
 void init_display (void)
 {
+	// Switch off all segments except for DP
 	PORTA &= ~_BV(SEG_DP);
 	PORTD &= 0x00;
 	
+	// Rotate all segments
 	delay_s(150);
 	PORTD &= _BV(SEG_F);
 	_delay_ms(150);
@@ -44,8 +46,10 @@ void init_display (void)
 	PORTD &= ~_BV(SEG_G);
 	PORTA |= _BV(SEG_DP);
 	_delay_ms(500);
+	// Light up all segments
 	PORTD &= 0xEF;
 	_delay_ms(500);
+	// Switch off DP
 	PORTA &= ~_BV(SEG_DP);
 }
 
@@ -55,7 +59,63 @@ void display_note (uint8_t note)
 	{
 		case NOTE_C:
 			PORTD = _BV(SEG_A) | _BV(SEG_D) | _BV(SEG_E) | _BV(SEG_F) & ~_BV(SEG_B) & ~_BV(SEG_C) & ~_BV(SEG_G);
+			LED_Sharp_OFF;
+			break;
+			
+		case NOTE_Cs:
+			PORTD = _BV(SEG_A) | _BV(SEG_D) | _BV(SEG_E) | _BV(SEG_F) & ~_BV(SEG_B) & ~_BV(SEG_C) & ~_BV(SEG_G);
+			LED_Sharp_ON;
+			break;
+			
+		case NOTE_D:
+			PORTD = _BV(SEG_B) | _BV(SEG_C) | _BV(SEG_D) | _BV(SEG_E) | _BV(SEG_G) & ~_BV(SEG_A) & ~_BV(SEG_F);
+			LED_Sharp_OFF;
+			break;
+			
+		case NOTE_Ds:
+			PORTD = _BV(SEG_B) | _BV(SEG_C) | _BV(SEG_D) | _BV(SEG_E) | _BV(SEG_G) & ~_BV(SEG_A) & ~_BV(SEG_F);
+			LED_Sharp_ON;
+			break;
+			
+		case NOTE_E:
+			PORTD = _BV(SEG_A) | _BV(SEG_D) | _BV(SEG_E) | _BV(SEG_F) | _BV(SEG_G) & ~_BV(SEG_B) & ~_BV(SEG_C);
+			LED_Sharp_OFF;
+			break;
 		
+		case NOTE_F:
+			PORTD = _BV(SEG_A) | _BV(SEG_E) | _BV(SEG_F) | _BV(SEG_G) & ~_BV(SEG_B) & ~_BV(SEG_C) & ~_BV(SEG_D);	
+			LED_Sharp_OFF;
+			break;
+			
+		case NOTE_Fs:
+			PORTD = _BV(SEG_A) | _BV(SEG_E) | _BV(SEG_F) | _BV(SEG_G) & ~_BV(SEG_B) & ~_BV(SEG_C) & ~_BV(SEG_D);
+			LED_Sharp_ON;
+			break;
+				
+		case NOTE_G:
+			PORTD = _BV(SEG_A) | _BV(SEG_C) | _BV(SEG_D) | _BV(SEG_E) | _BV(SEG_F) | _BV(SEG_G) & ~_BV(SEG_B);
+			LED_Sharp_OFF;
+			break;
+			
+		case NOTE_Gs:
+			PORTD = _BV(SEG_A) | _BV(SEG_C) | _BV(SEG_D) | _BV(SEG_E) | _BV(SEG_F) | _BV(SEG_G) & ~_BV(SEG_B);
+			LED_Sharp_ON;
+			break;
+			
+		case NOTE_A:
+			PORTD = _BV(SEG_A) | _BV(SEG_B) | _BV(SEG_C) | _BV(SEG_E) | _BV(SEG_F) | _BV(SEG_G) & ~_BV(SEG_D);
+			LED_Sharp_OFF;
+			break;
+			
+		case NOTE_As:
+			PORTD = _BV(SEG_A) | _BV(SEG_B) | _BV(SEG_C) | _BV(SEG_E) | _BV(SEG_F) | _BV(SEG_G) & ~_BV(SEG_D);
+			LED_Sharp_ON;
+			break;
+			
+		case NOTE_B:
+			PORTD = _BV(SEG_C) | _BV(SEG_D) | _BV(SEG_E) | _BV(SEG_F) | _BV(SEG_G) & ~_BV(SEG_A) & ~_BV(SEG_B);
+			LED_Sharp_OFF;
+			break;
 	}
 	
 }
